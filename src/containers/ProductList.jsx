7de444b1898050d4c9c2333a9a7d20389b185c86
pattopilco <pattopilco@gmail.com>
2022-09-15 18:00:50 -0500

    Custom Hooks2

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import ProductItem from '@components/ProductItem';
-import '@styles/ProductList.scss';
+import React from 'react';
+import ProductItem from '../components/ProductItem';
 import useGetProducts from '../hooks/useGetProducts';
+import '@styles/ProductList.scss';
+
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
@@ -10,14 +11,9 @@ const ProductList = () => {
     return (
         <section className='main-container'>
             <div className='ProductList'>
-                {
-                    products.map(
-                        (product) => (
-                            <ProductItem/>
-                    ))}
+            {products.map(product => <ProductItem product={product} key={product.id}/>)}
             </div>
         </section>
     );
 };
-
 export default ProductList;
